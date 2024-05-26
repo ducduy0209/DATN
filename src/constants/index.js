4caa677bdb8d144a612094aca04b17afb118c317
ducduy0209 <ducduy0209.work@gmail.com>
2024-05-27 00:58:20 +0700

    first commit for my final project

--- src/constants/index.js
+++
@@ -1,3 +0,0 @@
-exports.enumDuration = ['1 month', '3 months', '6 months', 'forever'];
-
-exports.enumPayBy = ['paypal', 'cash'];
--- src/jobs/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.cartJob = require('./cart.job');
-module.exports.emailJob = require('./email.job');
-module.exports.bookJob = require('./book.job');
-module.exports.analystJob = require('./analyst.job');
-module.exports.couponJob = require('./coupon.job');
-module.exports.affiliateJob = require('./affiliate.job');
